50ca94c9892d53c919623a8669fe950f0660dc97
Vitor Rodrigues de Marques <vitor.ifa@gmail.com>
2019-08-21 21:15:42 -0300

    dev

--- src/main/java/com/betinnapp/userservice/controller/UserController.java
+++ src/main/java/com/betinnapp/userservice/controller/UserController.java
@@ -2,11 +2,16 @@ package com.betinnapp.userservice.controller;
 
 import com.betinnapp.userservice.model.User;
 import com.betinnapp.userservice.service.UserService;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.UUIDDeserializer;
+import com.fasterxml.jackson.databind.ser.std.UUIDSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "user")
@@ -22,7 +27,19 @@ public class UserController {
 
     @PostMapping(path = "/")
     @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@Valid @RequestBody User user){
-        return userService.create(user);
+    public User createUser(@Valid @RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping(path = "/{uuid}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@PathVariable(value = "uuid") UUID id) {
+        userService.delete(id);
+    }
+
+    @PutMapping(path = "/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User updateUser(@Valid @RequestBody User user) {
+        return userService.save(user);
     }
 }
--- src/main/java/com/betinnapp/userservice/service/UserService.java
+++ src/main/java/com/betinnapp/userservice/service/UserService.java
@@ -5,15 +5,22 @@ import com.betinnapp.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.UUID;
+
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
 
-    public User create(User user) {
+    @Transactional
+    public User save(User user) {
         return userRepository.save(user);
     }
 
-
+    @Transactional
+    public void delete(UUID id) {
+        userRepository.deleteById(id);
+    }
 }
