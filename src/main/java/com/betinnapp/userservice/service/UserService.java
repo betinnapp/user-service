374cb6999308bf41d747e7909713b8945406a9ab
Matheus Rogge Cocia de Oliveira <matheus-rogge@hotmail.com>
2019-10-29 03:58:41 -0300

    Improve project architecture

--- src/main/java/com/betinnapp/userservice/service/UserService.java
+++ src/main/java/com/betinnapp/userservice/service/UserService.java
@@ -4,9 +4,11 @@ import com.betinnapp.userservice.model.User;
 import com.betinnapp.userservice.model.dto.UserDTO;
 import com.betinnapp.userservice.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.transaction.Transactional;
 import java.util.UUID;
@@ -14,29 +16,33 @@ import java.util.UUID;
 @Service
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    ObjectMapper mapper = new ObjectMapper();
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    public UserService(ObjectMapper objectMapper, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.objectMapper = objectMapper;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Transactional
     public UserDTO save(User user) {
         userRepository.save(user);
-        UserDTO userDTO = mapper.convertValue(user, UserDTO.class);
+        UserDTO userDTO = objectMapper.convertValue(user, UserDTO.class);
+
         return userDTO;
     }
 
     @Transactional
-    public UserDTO create(User user){
+    public UserDTO create(User user) {
         UUID random = UUID.randomUUID();
         user.setToken(random);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         userRepository.save(user);
 
-        UserDTO userDTO = mapper.convertValue(user, UserDTO.class);
+        UserDTO userDTO = objectMapper.convertValue(user, UserDTO.class);
         return userDTO;
     }
 
@@ -45,22 +51,22 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
-    public UserDTO findUserByToken(UUID token){
+    public UserDTO findUserByToken(UUID token) {
         User user = userRepository.findByToken(token);
-        UserDTO userDTO = mapper.convertValue(user, UserDTO.class);
+        UserDTO userDTO = objectMapper.convertValue(user, UserDTO.class);
+
         return userDTO;
     }
 
     @Transactional
-    public UserDTO updateUser(UUID token, User alterations){
-
+    public UserDTO updateUser(UUID token, User alterations) {
         User user = userRepository.findByToken(token);
         User alteredUser = userRepository.save(mergeAlterations(user,alterations));
 
-        return mapper.convertValue(alteredUser, UserDTO.class);
+        return objectMapper.convertValue(alteredUser, UserDTO.class);
     }
 
-    private User mergeAlterations(User user, User alterations){
+    private User mergeAlterations(User user, User alterations) {
         user.setFirstName(alterations.getFirstName());
         user.setLastName(alterations.getLastName());
         user.setShortName(alterations.getShortName());
